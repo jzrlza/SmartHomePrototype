61d80b3312afdba16f4183a30f2fe2efa90b0e84
jzrlza <parinvut12345@gmail.com>
2020-12-21 20:39:21 +0700

    More readable stages of the house.

--- extened_test_command.py
+++ extened_test_command.py
@@ -1,12 +1,16 @@
+current_time = 0
+
 time_dict = {
-        "เที่ยงคืน": "0:00"
-        "ตีหนึ่ง": "1:00"
+        "เที่ยงคืน": "0:00",
+        "ตีหนึ่ง": "1:00",
+        "เจ็ดโมงเช้า": "7:00",
+        "บ่ายโมง": "13:00",
+        "สามทุ่ม": "21:00"
     }
 
 class House:
     owner_name = ""
     rooms = {}
-    time = 0
 
     def __init__(self, owner_name):
         self.owner_name = owner_name
@@ -17,8 +21,9 @@ class House:
         self.rooms.update({room_str: room})
 
     def command(self, str_command):
+        print("Reading command : "+str_command)
         #Command reading, only works if the required words are at the start.
-        command = "none"
+        command = ""
         command_open_index = str_command.find("เปิดไฟ")
         command_close_index = str_command.find("ปิดไฟ")
         if command_open_index == 0 :
@@ -32,17 +37,35 @@ class House:
             if room in str_command:
                 room_name = room
                 break
+
+        #Scheduler, optional one
+        global time_dict
+        time_cmd = ""
+        for time in time_dict:
+            if time in str_command:
+                time_cmd = time
+                break
         
-        time = ""
+        #End if required texts are not present
+        if command == "" and room_name == "":
+            print("Invalid Command and Room!")
+            return
+        if command == "":
+            print("Invalid Command!")
+            return
+        if room_name == "":
+            print("Invalid Room!")
+            return
 
         #simple trigger without scheduled time
-        target_room = self.rooms[room_name]
-        if command == "turn_on_light":
-            target_room.trigger_light_on()
-        elif command =="turn_off_light":
-            target_room.trigger_light_off()
-        else:
-            print("error")
+        if time_cmd == "":
+            target_room = self.rooms[room_name]
+            if command == "turn_on_light":
+                target_room.trigger_light_on()
+            elif command =="turn_off_light":
+                target_room.trigger_light_off()
+
+        print("Done!")
 
     def print_all_rooms_stages(self):
         for room in self.rooms :
@@ -66,17 +89,44 @@ class Room:
 
     def print_stage(self):
         if self.light_open :
-            print(self.room_str+"ไฟเปิดอยู่")
+            print(self.room_name+" : light on")
         else :
-            print(self.room_str+"ไฟปิดแล้ว")
+            print(self.room_name+" : light off")
 
 
 house = House("My House")
 house.add_room("bed_room","ห้องนอน")
 house.add_room("bath_room","ห้องน้ำ")
 house.add_room("kitchen","ห้องครัว")
-print("Time 1")
+
+print(house.owner_name)
+
+print()
+print("Stage 1")
 house.print_all_rooms_stages()
+print()
+
 house.command("เปิดไฟห้องนอนหน่อย")
-print("Time 2")
+
+print()
+print("Stage 2")
 house.print_all_rooms_stages()
+print()
+
+house.command("ปิดไฟห้องนอนหน่อย")
+house.command("เปิดไฟห้องครัวหน่อย")
+
+print()
+print("Stage 3")
+house.print_all_rooms_stages()
+print()
+
+house.command("หน่อย")
+house.command("อะไร")
+house.command("ห้องครัว")
+house.command("ปิดไฟ")
+
+print()
+print("Stage 4")
+house.print_all_rooms_stages()
+print()
